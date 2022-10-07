2cf8da23860dd38c270834ca8aa642238e902d0c
CharithShali <shalitha.charith97@gmail.com>
2022-10-07 14:40:48 +0530

    new

--- rotaract/src/App.js
+++ rotaract/src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Article from './components/articles/Article';
+import Brand from './components/brand/Brand';
+import CTA from './components/cta/cta';
+import Feature from './components/feature/Feature';
+import Navbar from './components/navbar/navbar';
 
-function App() {
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+<h1>GTP</h1>
+
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ rotaract/src/components/cta/cta.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './cta.css';
+
+
+const cta = () => {
+  return (
+    <div>Brand</div>
+  )
+}
+
+export default cta
