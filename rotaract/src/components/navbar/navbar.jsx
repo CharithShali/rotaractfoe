aca0a6e2a4c6ca121d745d357809e7cd7f22636b
CharithShali <shalitha.charith97@gmail.com>
2022-10-27 07:25:43 +0530

    new

--- rotaract/src/components/navbar/navbar.jsx
+++ rotaract/src/components/navbar/navbar.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
 import './navbar.css';
+import {RiMenu3Line,RiCloseLin} from 'react-icons/ri';
+import logo from '../../assets/logo.png'
 
 const navbar = () => {
   return (
-    <div>Brand</div>
+    <div className='rotaract_navbar'>
+      <div className='rotaract_navbar-links'>
+      <div className='rotaract_navbar-links_logo'>
+        <img src={logo} alt='logo'/>
+      </div>
+      <div className='rotaract_navbar-links_container'>
+        <p><a href='#Home'>Home</a></p>
+        <p><a href='#rotaract'>What is Rotaract</a></p>
+        <p><a href='#branches'>Branches</a></p>
+        <p><a href='#contact'>Contact</a></p>
+      </div>
+      </div>
+      <div className='rotaract_navbar-sign'>
+        <p>Sign in</p>
+        <button type='button'>Sign Up</button>
+      </div>
+    </div>
   )
 }
 
--- rotaract/src/index.js
+++ rotaract/src/index.js
@@ -2,5 +2,6 @@ import React from 'react';
 import ReacrDOM from 'react-dom';
 
 import App from './App';
+import './index.css';
 
 ReacrDOM.render(<App />,document.getElementById('root'));
